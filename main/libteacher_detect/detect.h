b334c521b69c55943cede97f48a4e5ba006ac7a3
gunn <gnn2008@163.com>
2015-10-10 16:47:29 +0800

    添加json格式说明

--- main/libteacher_detect/detect.h
+++ main/libteacher_detect/detect.h
@@ -9,6 +9,19 @@ typedef struct detect_t detect_t;
 detect_t *det_open(const char *kvfname);
 void det_close(detect_t *det);
 
+/*
+	@param img: 图像
+	@return  ,json格式--   {"stamp":12345,"rect":[{"x":0,"y":0,"width":100,"height":100},
+	                                              {"x":0,"y":0,"width":100,"height":100}]}   
+	{
+		"stamp":12345,
+		"rect":
+		[
+			{"x":0,"y":0,"width":100,"height":100},
+			{"x":0,"y":0,"width":100,"height":100}
+		]
+	}
+ */
 const char *det_detect(detect_t *det);	// 返回json格式探测信息
 
 #ifdef __cplusplus
