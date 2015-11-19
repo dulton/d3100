7da04353d1376346dd238b237067091e1bba129a
zonekey <zonekey>
2015-11-19 03:23:36 -0500

    ..

--- main/libopencv_teacher_detect/hi_mat.h
+++ main/libopencv_teacher_detect/hi_mat.h
@@ -3,8 +3,6 @@
 
 class hiMat 
 {
-	int width_;
-	int height_;
 	int stride_;
 	unsigned int phy_addr_;
 	void *vir_addr_;
@@ -13,19 +11,20 @@ class hiMat
 	size_t ref_;		// 引用计数 ..
 
  public:
-	 hiMat();
+	hiMat();
 	// 开辟 mmz缓冲，初始化.
-	 hiMat(const cv::Mat & m);
+	hiMat(const cv::Mat & m);
 	~hiMat();
 	// 把数据赋于 m.
 	void download(cv::Mat & m);
 	// 为对象 开辟空间.
 	void create(int width, int height, int type);
-	 hiMat & operator =(const hiMat & src);
-	 hiMat & operator =(const cv::Mat & m);
+	hiMat & operator =(const hiMat & src);
+	hiMat & operator =(const cv::Mat & m);
 
-	int get_cols() const;
-	int get_rows() const;
+	int rows;
+	int cols;
+
 	int get_strie() const;
 	unsigned int *get_phy_addr_p() const;
 	void *get_vir_addr_p() const;
