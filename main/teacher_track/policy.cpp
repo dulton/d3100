0eb52d981850e597ee07fde1503249172238f8dd
谷娜娜 <gunana@gunanadeMacBook-Air.local>
2015-11-30 16:34:56 +0800

    add policy.cpp

--- main/teacher_track/policy.cpp
+++ main/teacher_track/policy.cpp
@@ -3,3 +3,28 @@
 
 // find_policy() 的实现放在 reg.cpp 中.
 
+Policy::Policy(const char* filename)
+{
+	kvc_ = kvc_open(filename);
+	current_policy_ = kvc_get(kvc_, "current_policy", "sp1");
+
+}
+
+void Policy::find_policy()
+{
+	if(current_policy_ == "sp1")
+	{
+		p1 p;
+		p.run();
+	}
+	else if(current_policy_ == "sp2")
+	{
+		p2 p;
+		p.run();
+	}
+	else if(current_policy_ == "sp3")
+	{
+		p3 p;
+		p.run();
+	}
+}
